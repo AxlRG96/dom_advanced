50fedc8617a0894b0b54d53fa0028b7d5e323311
Osmar Ramos <axl090696@gmail.com>
2021-07-20 16:50:49 -0500

    20/07/21 new feat | Actualizar funciones subtotal y total
    Se actualizarn las funciones updateSubtot y calcAll para pdoer calcular el total y el subtotal de los productos en la tabla de HTML

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,11 +1,27 @@
-let $cart = document.querySelector('#cart tbody');
-let $calc = document.getElementById('calc');
+let $cart = document.querySelector("#cart tbody");
+let $calc = document.getElementById("calc");
 
-function updateSubtot($product) {
+function updateSubtot(product) {
   // Iteration 1.1
+  //console.log(product)
+  let price = parseFloat(product.querySelector(".pu span").textContent);
+  let qty = parseFloat(product.querySelector(".qty input").value);
+  let subtotal = price * qty;
+  console.log("Subtotal= ", subtotal);
+  product.querySelector(".subtot span").innerHTML = subtotal;
+  return subtotal;
 }
 
 function calcAll() {
   // Iteration 1.2
+  let allProducts = Array.from(document.querySelectorAll(".product"));
+  //console.log(allProducts)
+  let total = 0;
+  for (numProduct in allProducts) {
+    total = updateSubtot(allProducts[numProduct]) + total;
+  }
+  document.querySelector("h2 span").innerHTML = total;
+  console.log("Total = ", total);
 }
+
 $calc.onclick = calcAll;
